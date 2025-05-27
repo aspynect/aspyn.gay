100918dcee9c7e0e33283f79d7c2d728b1914f84
aspynect <aspynect@gmail.com>
2025-05-26 20:31:13 -0500

    added blank php file for blog

---
+++ blog/blog_blank.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>title here</title>
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+</head>
+<body>
+    <?php require '../../assets/segments/nav.php';?>
+    <main>
+        article here
+    </main>
+    <?php require '../../assets/segments/footer.php';?>
+    <?php require '../../assets/segments/globalscripts.php';?>
+</body>
+</html>
