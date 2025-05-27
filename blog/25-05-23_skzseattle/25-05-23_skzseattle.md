db1b5ce45788accfb3a547af5747a5ad2361210b
aspynect <aspynect@gmail.com>
2025-05-26 20:31:27 -0500

    skz seattle blog

---
+++ blog/25-05-23_skzseattle/25-05-23_skzseattle.md
@@ -0,0 +1,22 @@
+# skz seattle 2025
+### 5/23/25
+### Words: 806
+
+## lifelong bucket list item ✅
+this weekend i got to see stray kids while visiting a friend in seattle!!! ive been wanting to do this for years, im so happy i finally got to
+
+## 00: seattle
+my first day in we did some tourism before we got in to the hotel. we went to the seattle aquarium, which was very cool but i also discovered i can get motion sick!! shoutouts to concave glass, very disorienting. we also went on the ferris wheel on the pier which was really cool, this place is beautiful and it was an amazing view. we got 5 rounds on the wheel even though it said we would get 3. we then whent on "wings of washington" which was a cool "4d" experience showing off a lot of scenery in the state. this wasnt the greatest idea given the aforementioned motion sickness but it was very fun.
+
+## 01: the hotel
+this shit is crazy. first off its expensive enough that im not being allowed to know how much it is so uh. shoutouts my friend's mom ! but its very nice, right next to the stadium, we can even peek in through the sides of it from our room. the best part though is that the rooftop pool has a view that we can peek into the stadium, and all day today (friday) the boys were rehearsing and we had a little party of people on the roof watching, it was like a cute pre-concert. we didnt manage to get our merch today bc the line was like 4 hours long but we'll get our stuff tomorrow :D im gonna be swagged out in my shit tomorrow. im hoping the losers that have been camping the past couple days dont get anything for actively going against what we were told but. idk lol 
+
+## 02: concert day!!
+oh my god im just getting back to this a couple days later and it was so miserable leading up. up at 4am, standing for hours until 8am when the line started, but we got incredibly lucky being at the right place at the right time, being near the line when it started. after another 2 hours the line started moving. took another hour or so to get our bands, and i ended up being #143 in pit 3, which was a lot better than we were expecting. we made some friends in the line around us that were super cool, and later around 3:30 we met up with them again for the line into the stadium. when we got in, we barely made it to the barricade just barely, i was the last person to get a barricade spot. i cannot overstate how long we spent standing between the different lines, but we made it to soundcheck!!! it was super cool. bangchan finished my finger heart during, it was super cute. later during the concert han waved at me and finished my heart, and towards the end hyunjin (my bias) stood on the step right in front of me, genuinely inches
+ away. it was super cool hearing the unit songs and some of the newer songs i hadnt heard before for the first time, and they played topline too!!! thats my favorite song of theirs :D towards the end when they played the [video project](https://youtu.be/KPUz3Iq_KbA) and brought out the [banners](https://x.com/Stray_Kids/status/1926526602952769989) that my friends worked on, it was super cute and it made me feel really happy for the community. after the concert we met up with friends and got to bed, beyond exhausted. when the concert adrenaline wore out i was incapable of standing on my feet, we crashed so hard lmao
+
+## 03: cupsleeve
+the morning after, we thankfully got some good rest and my body wasnt completely destroyed. i went to the cupsleeve event my friends were hosting, and it was wonderful. so many cool people handing out freebies still, and the other vendors were great. i also got a cute hat :D im not really one for hats but i like it a lot. made it to the airport that afternoon and said my goodbyes, it was hard but i know ill be back soon. 
+
+## 04: retrospect
+was it worth it? i think so. am i doing it again? absolutely not. we got so lucky to be able to get a good spot and if we hadn't it would have not been worth, if i ever come back to see them it'll be seated. i had a blast though, i got to spend good time with a close riend i dont see often, and i got to meet a ton of cool new people. gonna go down in my list some of the best weekends ive ever had
---
+++ blog/25-05-23_skzseattle/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aspyn blog: skz seattle 2025</title>
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+</head>
+<body>
+    <?php require '../../assets/segments/nav.php';?>
+    <main>
+        <h1 id="skz-seattle-2025">skz seattle 2025</h1>
+        <h3 id="section">5/23/25</h3>
+        <h3 id="words">Words 806</h3>
+        <h2 id="lifelong-bucket-list-item">lifelong bucket list item ✅</h2>
+        <p>this weekend i got to see stray kids while visiting a friend in
+        seattle!!! ive been wanting to do this for years, im so happy i finally
+        got to</p>
+        <h2 id="seattle">00: seattle</h2>
+        <p>my first day in we did some tourism before we got in to the hotel. we
+        went to the seattle aquarium, which was very cool but i also discovered
+        i can get motion sick!! shoutouts to concave glass, very disorienting.
+        we also went on the ferris wheel on the pier which was really cool, this
+        place is beautiful and it was an amazing view. we got 5 rounds on the
+        wheel even though it said we would get 3. we then whent on “wings of
+        washington” which was a cool “4d” experience showing off a lot of
+        scenery in the state. this wasnt the greatest idea given the
+        aforementioned motion sickness but it was very fun.</p>
+        <h2 id="the-hotel">01: the hotel</h2>
+        <p>this shit is crazy. first off its expensive enough that im not being
+        allowed to know how much it is so uh. shoutouts my friend’s mom ! but
+        its very nice, right next to the stadium, we can even peek in through
+        the sides of it from our room. the best part though is that the rooftop
+        pool has a view that we can peek into the stadium, and all day today
+        (friday) the boys were rehearsing and we had a little party of people on
+        the roof watching, it was like a cute pre-concert. we didnt manage to
+        get our merch today bc the line was like 4 hours long but we’ll get our
+        stuff tomorrow :D im gonna be swagged out in my shit tomorrow. im hoping
+        the losers that have been camping the past couple days dont get anything
+        for actively going against what we were told but. idk lol</p>
+        <h2 id="concert-day">02: concert day!!</h2>
+        <p>oh my god im just getting back to this a couple days later and it was
+        so miserable leading up. up at 4am, standing for hours until 8am when
+        the line started, but we got incredibly lucky being at the right place
+        at the right time, being near the line when it started. after another 2
+        hours the line started moving. took another hour or so to get our bands,
+        and i ended up being #143 in pit 3, which was a lot better than we were
+        expecting. we made some friends in the line around us that were super
+        cool, and later around 3:30 we met up with them again for the line into
+        the stadium. when we got in, we barely made it to the barricade just
+        barely, i was the last person to get a barricade spot. i cannot
+        overstate how long we spent standing between the different lines, but we
+        made it to soundcheck!!! it was super cool. bangchan finished my finger
+        heart during, it was super cute. later during the concert han waved at
+        me and finished my heart, and towards the end hyunjin (my bias) stood on
+        the step right in front of me, genuinely inches away. it was super cool
+        hearing the unit songs and some of the newer songs i hadnt heard before
+        for the first time, and they played topline too!!! thats my favorite
+        song of theirs :D towards the end when they played the <a
+        href="https://youtu.be/KPUz3Iq_KbA">video project</a> and brought out
+        the <a
+        href="https://x.com/Stray_Kids/status/1926526602952769989">banners</a>
+        that my friends worked on, it was super cute and it made me feel really
+        happy for the community. after the concert we met up with friends and
+        got to bed, beyond exhausted. when the concert adrenaline wore out i was
+        incapable of standing on my feet, we crashed so hard lmao</p>
+        <h2 id="cupsleeve">03: cupsleeve</h2>
+        <p>the morning after, we thankfully got some good rest and my body wasnt
+        completely destroyed. i went to the cupsleeve event my friends were
+        hosting, and it was wonderful. so many cool people handing out freebies
+        still, and the other vendors were great. i also got a cute hat :D im not
+        really one for hats but i like it a lot. made it to the airport that
+        afternoon and said my goodbyes, it was hard but i know ill be back
+        soon.</p>
+        <h2 id="retrospect">04: retrospect</h2>
+        <p>was it worth it? i think so. am i doing it again? absolutely not. we
+        got so lucky to be able to get a good spot and if we hadn’t it would
+        have not been worth, if i ever come back to see them it’ll be seated. i
+        had a blast though, i got to spend good time with a close riend i dont
+        see often, and i got to meet a ton of cool new people. gonna go down in
+        my list some of the best weekends ive ever had</p>
+    </main>
+    <?php require '../../assets/segments/footer.php';?>
+    <?php require '../../assets/segments/globalscripts.php';?>
+</body>
+</html>
