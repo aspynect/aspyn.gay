7aabb0eee90d599418bac77a04d7f13d24e19626
aspynect <aspynect@gmail.com>
2026-01-02 02:08:04 -0600

    year of the setup

---
+++ blog/26-01-01_2025_year_of_setup/26-01-01_2025_year_of_setup.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>title here</title>
+    <link rel="stylesheet" href="/styles.css">
+    <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+</head>
+<body>
+    <?php require '../../assets/segments/nav.php';?>
+    <main>
+        <h1 id="the-year-of-setup">2025: the year of setup</h1>
+        <h3 id="section">1/1/26</h3>
+        <h3 id="words-1639">Words: 1,639</h3>
+        <h2 id="i-was-right">00: i was right</h2>
+        <blockquote>
+        <p>when i say “this is my year” i dont expect it to be perfect. it’s
+        probably gonna be pretty shit in all honesty. but this is my year to
+        grow.</p>
+        </blockquote>
+        <p>at the start of this year, i said this was my year, and that you
+        can’t skip the process, and this year sure was full of process. overall?
+        pretty shit year if you look at the small picture, but i’ve never felt
+        this set up to have an amazing year going into 2026.</p>
+        <h2 id="recap">01: recap</h2>
+        <h3 id="bugmush">bugmush</h3>
+        <p>on january 6th i joined <a
+        href="https://bsky.app/profile/bugmu.sh">bugmush</a>, mainly to hang out
+        with my friends but it also was going to help me with my <a
+        href="https://aspyn.gay/blog/24-12-21_2025_sidequests/">sidquest</a> to
+        stop being scared of the DAW. while i didnt make nearly as much progress
+        in this department, it helpedme feel out what i want to get out of music
+        and what fulfillment i like to see from it. turns out? production isn’t
+        really for me. but i loveee writing and vocals, and that is something i
+        want to pursue further.</p>
+        <p>beyond that, this is where i met so many amazing friends this year,
+        as well as a particularly special someone i’ll talk about more later</p>
+        <h3 id="chicago">chicago</h3>
+        <p>in february, i spontaneously flew over to chicago to spend two days
+        with my then-girlfriend and <a
+        href="https://bsky.app/profile/cinny.umm.gay">my wife</a>, along with a
+        metamour. while it was a nice weekend, i do not recommend sleeping 4
+        people to a bed.</p>
+        <p>in march, i then went back, this time driving with my college friend
+        <a href="https://bsky.app/profile/kennecfox.bsky.social">kenji</a> to
+        see saoirse dream, fraxiom, and leastfavorite! perform live. while there
+        i got to meet more of my friends i’d met in bugmush. it was the first
+        show i’ve ever been to, and it was so incredible, and it made me want
+        more. again an amazing day, but i also do not recommend daytripping a 6
+        hour drive each way on a school night.</p>
+        <h3 id="iowa-moment">iowa moment</h3>
+        <p>i, along with some friends, went to protest a bill at the iowa
+        capitol that threatened to remove gender identity as a protected group
+        under the state civil rights code. there was so much turnout but
+        unfortunately nothing could stop that hellish state’s legislation along
+        side kim reynolds (fuck kim reynolds) from doing the evils that they
+        were determined to do. i do not recommend losing your civil rights on a
+        thursday afternoon— this accelerated my graduation plan greatly.</p>
+        <h3 id="england-pt.1">england (pt.1)</h3>
+        <p>also in march, i flew out during my spring break to see my wife in
+        england. this week was incredible— i fell in love with tesco mac &amp;
+        cheese and greggs sausage rolls. it was a hard week but was amazing. i
+        do not recommend having 6 people staying in a 2 bed apartment even for
+        just a week.</p>
+        <h3 id="food-house-pt.1">food house (pt.1)</h3>
+        <p>in may i went to food house shows in minneapolis and milwaukee, and
+        had a blast both times. i bought a cd at the first show and had them
+        sign it, and then shocked them by showing up at the next show the next
+        night. i don’t recommend physically exerting yourself to your limit two
+        nights in a row, but sometimes the fun is worth it. live your happiest
+        life.</p>
+        <h3 id="graduation">graduation</h3>
+        <p><a href="https://aspyn.gay/blog/25-05-18_graduation/">i
+        graduated</a>. i don’t recommend spending hundreds of thousands on a
+        piece of paper that may not help you in any way</p>
+        <h3 id="stray-kids">stray kids</h3>
+        <p><a href="https://aspyn.gay/blog/25-05-26_skzseattle/">i went to a
+        stray kids concert</a>. i don’t recommend standing all day in the hot
+        sun for a chance at good seats, but it’s worth it once right?</p>
+        <h3 id="gdq">gdq</h3>
+        <p>sgdq 2025… happened. im going to glaze over this for many reasons
+        good and bad.</p>
+        <h3 id="england-pt.-2">england (pt. 2)</h3>
+        <p>in august, i booked a one way ticket to england with the intention of
+        coming back Sometime. i had some plans but not a whole lot of them, i
+        was just taking my time and living happiness. this would end up being a
+        3 month trip that would be so fun, so overwhelming, and would enlighten
+        me to a lot of things that have made me feel much more prepared for the
+        future.</p>
+        <p>i met a lot of wonderful people while i was there, some of the first
+        being cinny’s parents. who. we are not entirely sure if they know she’s
+        poly. but i did accidentally meow in front of them within 10 minutes of
+        them being there. and they did think it was yoru (hana’s cat).</p>
+        <p>but, within the first week or so of me being there, the food house
+        show we’d planned months prior happened. that night was amazing in a way
+        i never could have predicted and that i could never forget. there, i met
+        síoda irl for the first time. i had fully prepared myself to stand at
+        the back of the show and enjoy the music like i usually did at shows
+        like this, but she grabbed me by the hand and dragged me out into the
+        crowd. singing and dancing with her that night was maybe the most ive
+        ever let myself feel alive in a moment, and the rest of the weekend was
+        more amazing than i ever could have planned for.</p>
+        <p>i’ve learned over the years that my personality disorder is to be
+        taken seriously so i was soooo patient and normal . but i <em>did</em>
+        have her back over a month later. for one of the first times ever i went
+        on a real date, and after a whole evening of being too shy to ask her
+        out, i finally did it. i could talk about her forever, so i won’t get
+        going too far, but i love her and she has been so wonderful for me in a
+        way that i couldn’t even begin to explain.</p>
+        <p>…then i had her over a third time when we went to the saoirse
+        dream/canofbliss/russelbuck show with cinny and eden. oh my goodness
+        that was wonderful, it is so fun to sing and dance with people you
+        love.</p>
+        <p>i also got to see my best friend! two whole different times while i
+        was there! that was so amazing and so cool and it was amazing and epic
+        and its so cool.</p>
+        <p>i got to go see a bunch of oomfs while i was there…. too much to talk
+        about here. but it was epic</p>
+        <p>incidentally at the same time some Events that are too much to talk
+        about publicly led to me talking again with some friends that i hadn’t
+        talked to in a while.</p>
+        <p>i got to reconnect with my wonderful friend tau, who i even got to go
+        visit before i left the country. we hung out and started watching dragon
+        ball and went to its fgc locals. my train back to sheffield got
+        cancelled :D so i had to stay an extra night . oh noooo</p>
+        <p>but also i got to reconnect with nero, someone who i was very close
+        to a couple years ago but were kept apart due to Reasons :tm:, and it
+        was incredible to get to talk again under circumstances that we could
+        actually be open and ourselves. i fell in love with them a second time,
+        and a few days after getting home, at the start of december, we started
+        dating.</p>
+        <h3 id="the-end-of-the-year">the end of the year</h3>
+        <p>just last night, i went up to the cities and spent new years with my
+        lovely friend autumn and its local friends. i spent a lot of time
+        thinking and reflecting on the year, which led me to this post.</p>
+        <h2 id="now">02: now</h2>
+        <p>right now, i feel like i’ve set myself up for the best position i’ve
+        ever been in. i have three amazing partners who i love so dearly, an
+        amazing support system of friends who have my back, i’m starting a part
+        time job soon, and overall i’m looking forward to this year and the
+        future as a whole in a way i never have before.</p>
+        <h2 id="the-new-year">03: the new year</h2>
+        <p>as we know, i hate resolutions. resolutions tend to be peoples’
+        shallow goals that they think they can just Do and keep with a bit of
+        determination. this year i’m reprising everyone’s favorite: we got
+        sidequests baybeeeeeeee</p>
+        <h2 id="a-love-right">03a: love right</h2>
+        <p>i have so much love in my life right now, and this year i want to
+        love with my whole heart. no compromises, no fear, and no reservations.
+        im going to see my partners this year and even when they’re far away im
+        going to do everything i can to overcome my fears and love like i never
+        have before.</p>
+        <h2 id="b-reclaim-music">03b: reclaim music</h2>
+        <p>in the past couple years, because of some people who fundamentally
+        shaped my music taste turning out to be quite terrible people, listening
+        to music has become significantly harder and it has not had its same
+        place in my day to day life that it used to. i want to reclaim this and
+        start listening to music more, and i want to be able to love it like i
+        used to.</p>
+        <h2 id="c-make-more-music">03c: make more music</h2>
+        <p>i’ve written and recorded on little practice beats friends have made,
+        and i’ve made a MYOM song, and i want to do this more. i want to
+        practice and get better and get to the point that maybe i can have more
+        confidence in doing collaborations.</p>
+        <h2 id="d-finish-dragon-ball">03d: finish dragon ball</h2>
+        <p>admittedly, tau and i committed to watch all of dragon ball before we
+        realized how big of a committment it was. irregardless we have
+        committed, and i’ve downloaded the Seed of Might color corrections of
+        the OG series and the db/dbz movies. we’re hoping to finish it this
+        year, and it’s exciting.</p>
+        <h2 id="conclusion">conclusion</h2>
+        <p>last year set me up real good to have the best year ever this year.
+        im gonna make the most of it.</p>
+    </main>
+    <?php require '../../assets/segments/footer.php';?>
+    <?php require '../../assets/segments/globalscripts.php';?>
+</body>
+</html>
