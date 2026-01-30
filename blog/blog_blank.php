300f231e8cb46d3b1013d1c5491b1bd7a2639bce
aspynect <aspynect@gmail.com>
2026-01-29 18:25:43 -0600

    update blank for meta tags

--- blog/blog_blank.php
+++ blog/blog_blank.php
@@ -6,6 +6,12 @@
     <title>title here</title>
     <link rel="stylesheet" href="/styles.css">
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
+    <meta property="og:title" content="article title" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://aspyn.gay/blog/(name)" />
+    <meta property="og:image" content="https://aspyn.gay/assets/favicon.png" />
+    <meta property="og:description" content="" />
+    <meta name="theme-color" content="#FF00FF" />
 </head>
 <body>
     <?php require '../../assets/segments/nav.php';?>
